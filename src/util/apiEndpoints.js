8ddd14a1fbb5d1b7e12d0aa5db06949e3215ac0e
Jeet <72684295+Jeet1243@users.noreply.github.com>
2025-09-20 18:00:44 +0530

    Update apiEndpoints.js

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:8080/api/v1.0";
-const BASE_URL = "https://cloudshare-backend-jeet.up.railway.app/";
+const BASE_URL = "https://cloudshare-backend-jeet.up.railway.app/api/v1.0";
 export const apiEndpoints = {
     FETCH_FILES: `${BASE_URL}/files/my`,
     GET_CREDITS: `${BASE_URL}/users/credits`,
